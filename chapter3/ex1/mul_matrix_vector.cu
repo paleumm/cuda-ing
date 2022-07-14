1d6d2e039d5fb67c6a66aa21da20c67fbd571498
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-07-15 02:26:00 +0700

    Create mul_matrix_vector.cu

---
+++ chapter3/ex1/mul_matrix_vector.cu
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mul_matrix_vector.cu
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  15/07/2022 02:22:15
+ *       Revision:  none
+ *       Compiler:  nvcc
+ *
+ *         Author:  Permpoon B (pb)
+ *        Company:  none
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+
+__global__ void func(void);
+
+int main(int argc, char* argv[]){
+    // code section
+    
+
+    return 0;
+}
