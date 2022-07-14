48f6dcb39d7e6ab876c3bc79f4ba5d7fdcd6d9a1
Permpoon Boonyarit <zdotqdot@gmail.com>
2022-07-15 02:21:03 +0700

    Update add_matrix.cu

--- chapter3/ex1/add_matrix.cu
+++ chapter3/ex1/add_matrix.cu
@@ -33,13 +33,24 @@ __global__ void add_mat_kernel_1(float *a, float *b, float *c, int n){
     if(index < n*n) c[index] = a[index] + b[index]; 
 }
 
-// __global__ void add_mat_kernel_2(float *a, float *b, float *c, int n){
-//     int index = blockIdx.x * blockDim.x + threadIdx.x;
-//     if(index < n)
-//         for (size_t i = 0; i < n; ++i)
-//             c[index+i] = a[index+i] + b[index+i];
-// }
+__global__ void add_mat_kernel_2(float *a, float *b, float *c, int n){
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if(index < n*n){
+        for (size_t i = 0; i < n; ++i){
+            c[index+i] = a[index+i] + b[index+i];
+        }
+    }
+}
 
+__global__ void add_mat_kernel_3(float *a, float *b, float *c, int n){
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    if(index < n*n){
+        for (size_t i = 0; i < n; i++){
+            int offset = n * i + index;
+            c[offset] = a[offset] + b[offset];
+        }  
+    }
+}
 
 /*
  * =====================================================================================
@@ -54,6 +65,7 @@ __global__ void add_mat_kernel_1(float *a, float *b, float *c, int n){
 __host__ void add_matrix(float *h_out, float *h_first, float *h_second, int n, int mode){
     // size of matrix
     int size = n * n * sizeof(float);
+    int dim = n * n;
     float *d_first, *d_second, *d_out;
 
     cudaError_t errFirst = cudaMalloc((void**)&d_first, size);
@@ -79,15 +91,16 @@ __host__ void add_matrix(float *h_out, float *h_first, float *h_second, int n, i
     switch (mode)
     {
     case 1:
-        add_mat_kernel_1<<<ceil(n/64.0),64>>>(d_first, d_second, d_out, n);
+        add_mat_kernel_1<<<ceil(dim/1024.0),1024>>>(d_first, d_second, d_out, n);
         break;
     case 2:
-        // add_mat_kernel_2<<<ceil(n/64.0),64>>>(d_first, d_second, d_out, n);
+        add_mat_kernel_2<<<ceil(dim/1024.0),1024>>>(d_first, d_second, d_out, n);
         break;
     case 3:
+        add_mat_kernel_3<<<ceil(dim/1024.0),1024>>>(d_first, d_second, d_out, n);
         break;
     default:
-        add_mat_kernel_0<<<ceil(n/64.0),64>>>(d_first, d_second, d_out, n);
+        add_mat_kernel_0<<<ceil(dim/1024.0),1024>>>(d_first, d_second, d_out, n);
         break;
     }
 
@@ -104,21 +117,22 @@ int main(int argc, char* argv[]){
 
     if(mode > 3){printf("mode should be 0-3\n");return 1;}
 
-    int size = n*n;
-
+    //int size = n*n;
+    int size = 1024;
     // initialize matrix
     float A[size], B[size], C[size];
     for (size_t i = 0; i < size; i++)
     {
         A[i] = i % 10;
         B[i] = (i+1) % 10;
+        C[i] = 0;
     }
     //for (int i = 0; i < size; i++) printf("A[%d] = %f, B[%d]=%f\n",i,A[i],i,B[i]);
     
     add_matrix(C, A, B, n, mode);
-    for (size_t i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        printf("%.0f + %.0f = %.0f\n", A[i], B[i], C[i]);
+        printf("%d %.0f + %.0f = %.0f\n", i, A[i], B[i], C[i]);
     }
     return 0;
 }
